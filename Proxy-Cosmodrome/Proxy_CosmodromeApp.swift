9b9736e9b7fa0f384f72d6d5bc9c3a4c292571bc
alwin <alwintv5018@gmail.com>
2026-05-03 19:49:00 +0530

    Initial Commit

---
+++ Proxy-Cosmodrome/Proxy_CosmodromeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proxy_CosmodromeApp.swift
+//  Proxy-Cosmodrome
+//
+//  Created by Alwin T Varghese on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Proxy_CosmodromeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Proxy-CosmodromeTests/Proxy_CosmodromeTests.swift
@@ -0,0 +1,19 @@
+//
+//  Proxy_CosmodromeTests.swift
+//  Proxy-CosmodromeTests
+//
+//  Created by Alwin T Varghese on 03/05/26.
+//
+
+import Testing
+@testable import Proxy_Cosmodrome
+
+struct Proxy_CosmodromeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
